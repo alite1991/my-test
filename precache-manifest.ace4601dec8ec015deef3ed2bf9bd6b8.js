b3859573fb3c59e706512d87cf59da545e931618
Alireza Tehrani <alite1991@users.noreply.github.com>
2021-10-07 23:56:10 +0330

    Updates

---
+++ precache-manifest.ace4601dec8ec015deef3ed2bf9bd6b8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b10ffe7c433d35114e4b9095d1cb1c6c",
+    "url": "/my-test/index.html"
+  },
+  {
+    "revision": "026487e6693b55d79e02",
+    "url": "/my-test/static/js/2.b6f1219b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my-test/static/js/2.b6f1219b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "070c4f230b5584c75c77",
+    "url": "/my-test/static/js/main.9af79aca.chunk.js"
+  },
+  {
+    "revision": "23e93da251ecc4c37679",
+    "url": "/my-test/static/js/runtime-main.04af1658.js"
+  }
+]);
